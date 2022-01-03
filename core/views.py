15fb3200e6fa5788798c1475c06a281aeb2a4c5e
Sreebash Chandra Das <sreebashdas.1@gmail.com>
2022-01-03 18:16:11 +0600

    added category serializers

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from core.models import Category
+from core.serializers import CategorySerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
